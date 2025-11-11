fa81a3b3a0f2a1b5804a62dab2778eacc8088cbd
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-11-11 11:01:26 -0300

    so ajeitando id

--- src/models/estoqueModel.js
+++ src/models/estoqueModel.js
@@ -1,9 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
   const EstoqueModel = sequelize.define('EstoqueModel', {
-    id: { type: DataTypes.BIGINT, allowNull: true },
-    quantidade: { type: DataTypes.DECIMAL, allowNull: false },
-    armazem: { type: DataTypes.INTEGER, allowNull: false },
-    semente: { type: DataTypes.INTEGER, allowNull: false },
-  }, { tableName: 'estoquemodels', timestamps: true });
+    id: { 
+      type: DataTypes.BIGINT, 
+      allowNull: false,       
+      primaryKey: true,       
+      autoIncrement: true    
+    },
+    quantidade: { 
+      type: DataTypes.DECIMAL, 
+      allowNull: false 
+    },
+    armazem: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false 
+    },
+    semente: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false 
+    },
+  }, { 
+    tableName: 'estoquemodels', 
+    timestamps: true 
+  });
+
   return EstoqueModel;
 };
